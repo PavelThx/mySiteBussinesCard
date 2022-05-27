7febe87443dbec5ba3709f8ff40308022ea6d902
PavelThx <105968027+PavelThx@users.noreply.github.com>
2022-05-27 17:20:10 +0300

    Delete mysite directory

--- mysite/script.js
+++
@@ -1,5 +0,0 @@
-let page = document.querySelector(".page");
-let themeButton = document.querySelector(".theme-button");
-themeButton.onclick = function () {
-  page.classList.toggle("dark-theme");
-};
